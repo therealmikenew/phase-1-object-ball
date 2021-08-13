d4ee814336b19b7da67c305cc44e38ea774ee19c
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-13 10:48:48 -0400

    building object

---
+++ src/00-objectball.js
@@ -0,0 +1,43 @@
+function gameObject() {
+
+ const obj = {
+    home: {
+        teamName: "",
+        colors: [],
+        players: {
+            "Alan Anderson": {
+            number: 
+            shoe:
+            points:
+            rebounds:
+            assists:
+            steals:
+            blocks:
+            slamDunks:
+            }
+        },
+
+
+    },
+    away: {
+        teamName: "",
+        colors: [],
+        players: {
+            "Jeff Adrien": {
+            number: 
+            shoe:
+            points:
+            rebounds:
+            assists:
+            steals:
+            blocks:
+            slamDunks:
+            }
+        }
+    }
+}
+ 
+return obj;
+
+}
+
